710486ca87bcea5b3af934fbb0a5c9bf5cefe599
chillguy7789-ops <chillguy7789@gmail.com>
2026-03-07 16:53:53 +0530

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,9 @@
 import { cors } from "hono/cors";
 import { env } from "./env.js";
 
-const DEFAULT_ALLOWED_ORIGINS = ["http://localhost:4000", "*"];
-
-const allowedOrigins = env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
-    ? env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-    : DEFAULT_ALLOWED_ORIGINS;
-
 export const corsConfig = cors({
-    allowMethods: ["GET"],
+    allowMethods: ["GET", "OPTIONS"],
     maxAge: 600,
-    credentials: true,
-    origin: allowedOrigins,
+    credentials: false,
+    origin: "*",
 });
