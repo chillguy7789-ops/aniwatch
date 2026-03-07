73b4d7117e74bedf7c9ac1615a099a3e642a7d2b
chillguy7789-ops <chillguy7789@gmail.com>
2026-03-07 16:10:08 +0530

    Update proxy.ts

--- src/routes/proxy.ts
+++ src/routes/proxy.ts
@@ -2,6 +2,18 @@ import { Hono } from "hono";
 
 const proxyRouter = new Hono();
 
+// Handle preflight for null-origin local file requests
+proxyRouter.options("/m3u8", (c) => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+    },
+  });
+});
+
 proxyRouter.get("/m3u8", async (c) => {
   const url = c.req.query("url");
   const referer = c.req.query("referer") || "https://megacloud.tv/";
@@ -22,6 +34,8 @@ proxyRouter.get("/m3u8", async (c) => {
       headers: {
         "Content-Type": "application/vnd.apple.mpegurl",
         "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
       },
     });
   } catch (e: any) {
